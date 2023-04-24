0966fceda2443ac09e3d005a32b130a22c867405
techsavvysos <kedogosospeter36@gmail.com>
2023-04-24 21:25:40 +0100

    added click functionality to drum pads

--- src/components/DrumMachine/DrumMachine.js
+++ src/components/DrumMachine/DrumMachine.js
@@ -1,15 +1,35 @@
 import React from "react";
 import './DrumMachine.css'
-import PadBank from "../pad-bank/PadBank";
 import Controls from "../controls/Controls";
-import {FaFireAlt} from 'react-icons/fa'
+import {FaFireAlt} from 'react-icons/fa';
+import { useState } from "react";
 
 export default function DrumMachine() {
+
+    const [dispayText, setDisplayText] = useState("");
+
+    const handleButtonClick = (text, audioSrc) => {
+        setDisplayText(text);
+        let id = document.getElementById(audioSrc).src;
+        new Audio(id).play();
+    }
+
     return(
         <div id="drum-machine" className="drum-machine">
-            <PadBank />
+
+            <div className="drumPads">
+                <div className="drum-pad" onClick={() => {handleButtonClick("Heater-1", "Q")}} ><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" className="clip" id="Q"></audio>Q</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Heater-2", "W")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" className="clip" id="W"></audio>W</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Heater-3", "E")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" className="clip" id="E"></audio>E</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Heater-4", "A")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3" className="clip" id="A"></audio>A</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Heater-6", "S")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"className="clip" id="S"></audio>S</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Dsc_oh", "D")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3" className="clip" id="D"></audio>D</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Kick_n_Hat", "Z")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3" className="clip" id="Z"></audio>Z</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("RP4_KICK_1", "X")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" className="clip" id="X"></audio>X</div>
+                <div className="drum-pad" onClick={() => {handleButtonClick("Cev_H2", "C")}}><audio src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" className="clip" id="C"></audio>C</div>
+            </div>
             <FaFireAlt className="logo"/>
-            <Controls />
+            <Controls text = {dispayText}/>
         </div>
     );
 }
--- src/components/controls/Controls.js
+++ src/components/controls/Controls.js
@@ -1,15 +1,19 @@
 import React from "react";
-import './controls.css'
+import './controls.css';
 
-export default function Controls() {
+
+
+export default function Controls(props) {
     return(
         <div className="control-tab">
             <div className="switch">
                 <p>Power</p>
-                <div className="switch-button"></div>
+                <div className="switch-button" >
+                    <div className="switch-slider"></div>
+                </div>
             </div>
-            <div className="whitespace"></div>
-            <input type="range" max={1} min={0} step={0.01} value={0.32} />
+            <div className="whitespace" id="display">{props.text}</div>
+            <input type="range" max={1} min={0} step={0.01} value={0.32} className="volume"/>
         </div>
     );
 }
